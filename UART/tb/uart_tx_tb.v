f462ac4d106a4cb04405b0c037a1a9de9e857a4b
WoLf4905 <akashdhyan49052@gmail.com>
2026-06-11 19:03:07 +0000

    created testbench for the uart transmitter

---
+++ UART/tb/uart_tx_tb.v
@@ -0,0 +1,50 @@
+module uart_tx_tb;
+
+reg clk;
+reg reset;
+reg start;
+reg [7:0] data_in;
+
+wire tx;
+
+
+uart_tx dut(
+    .clk(clk),
+    .reset(reset),
+    .start(start),
+    .data_in(data_in),
+    .tx(tx)
+);
+
+
+
+always #10 clk = ~clk;
+
+initial
+begin
+    clk=0;
+    reset=1;
+    start=0;
+    data_in=8'h00;
+
+    #100;
+    reset=0;
+
+    #100;
+    data_in=8'h41;
+
+    start=1;
+    #20;
+    start=0;
+
+    #1000000;
+
+    $finish;
+end
+
+initial
+begin   
+    $dumpfiles("uart.vcd");
+    $dumpvars(0,uart_tx_tb);
+end
+endmodule
